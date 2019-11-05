fd10ec29461bfd06b71591a7e95dd358a494f068
Vicfred <vicfredboy@gmail.com>
2019-11-05 17:54:47 -0600

    names

--- Sesion-03/Ejemplo-03/demo/src/test/java/com/example/demo/ControladorTests.java
+++ Sesion-03/Ejemplo-03/demo/src/test/java/com/example/demo/ControladorTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class JUnitControllerTests {
+public class ControladorTests {
     @Test
     public void testControlador() {
         Controlador controlador = new Controlador();
